9a77a6ec0ee3b5e7a830f2deb291716612f781b7
Miguel Aragon <luizaragon25@gmail.com>
2023-11-28 10:15:52 -0600

    vista Codigo Correo

--- backend-Admin/resources/views/Mail/validacionUsuarioAdmin.blade.php
+++ backend-Admin/resources/views/Mail/validacionUsuarioAdmin.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!--<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -15,4 +15,28 @@
 
 </body>
 
-</html>
+</html>-->
+
+
+<!DOCTYPE html> 
+<html lang="en"> 
+    <head> 
+        <meta charset="UTF-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>One Time Password</title>
+         <style>
+         body { font-family: Arial, sans-serif; background-color: #666060; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; } 
+         .otp-container { background-color: #f7f7f7; padding: 30px; border-radius: 5px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); text-align: center; } 
+         .otp-container h1 { margin: 0; color: #000000; } 
+         .otp-container p { margin: 0; color: #666666; } 
+         .otp-code { font-size: 24px; font-weight: bold; color: #333333; } </style> 
+         </head> 
+         <body> 
+            <div class="otp-container"> 
+                <h1>Codigo de verificación</h1> 
+                <p>es: </p>
+                 <p class="otp-code">{{ $codigoVerificacion }}
+                </p> 
+                </div>
+             </body>
+              </html>
